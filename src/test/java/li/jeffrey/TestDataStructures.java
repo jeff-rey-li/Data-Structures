0141a3d0928bdc771a98327b04662dfaf613f7ee
Jeffrey Li <fballjeff@gmail.com>
2020-07-29 16:56:39 -0500

    Created new test file

--- src/test/java/li/jeffrey/TestDataStructures.java
+++
@@ -1,4 +0,0 @@
-package li.jeffrey;
-
-public class TestDataStructures {
-}
---
+++ src/test/java/li/jeffrey/binarytrees/TestBinarySearchTree.java
@@ -0,0 +1,18 @@
+package li.jeffrey.binarytrees;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestBinarySearchTree {
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+    @Test
+    public void test001() {
+
+    }
+
+}
